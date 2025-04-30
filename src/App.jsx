5335ac710155ec488542e816f33271362f2ec2a8
Andres Holguin <andresmholguin@gmail.com>
2025-04-30 00:06:24 -0500

    Mejora de estilos y estructura en la interfaz de usuario, incluyendo ajustes en colores y clases de utilidad (mobile-design).

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,15 @@ function App() {
           <span>tter</span>
         </h1>
       </header>
-      <main className="w-[375px] h-[700px] bg-White rounded-2xl px-[32px] pt-[24px]">
-        <section className="mb-8">
+      <main className="w-[375px] bg-White rounded-2xl px-[32px] py-[6px]">
+        <section className="mb-8 mt-4">
           <label className="text-sm text-Grey-500 mb-3 block" htmlFor="bill">
             Bill
           </label>
           <div className="flex items-center relative rounded-sm">
             <img className="absolute ml-4" src={dollar} alt="Icon dollar" />
             <input
-              className="text-right bg-Grey-100 text-Green-900 rounded-md h-[48px] w-full pr-4"
+              className="input h-[48px] w-full focus:inputFocus"
               type="text"
               id="bill"
               placeholder="20"
@@ -26,40 +26,46 @@ function App() {
           </div>
         </section>
         <section className="mb-8">
-          <label className="text-sm text-Grey-500 mb-3 block" htmlFor="">
+          <label
+            className="text-sm text-Grey-500 mb-3 block"
+            htmlFor="selectTip"
+          >
             Select Tip %
           </label>
-          <div className="grid grid-cols-2 gap-4 text-White">
-            <button className="bg-Green-900 rounded-md p-1">5%</button>
-            <button className="bg-Green-900 rounded-md p-1">10%</button>
-            <button className="bg-Green-900 rounded-md p-1">15%</button>
-            <button className="bg-Green-900 rounded-md p-1">25%</button>
-            <button className="bg-Green-900 rounded-md p-1">50%</button>
+          <div className="grid grid-cols-2 gap-4 text-White" id="selectTip">
+            <button className="btnPorcent">5%</button>
+            <button className="btnPorcent">10%</button>
+            <button className="btnSelect">15%</button>
+            <button className="btnPorcent">25%</button>
+            <button className="btnPorcent">50%</button>
             <input
-              className="bg-Grey-100 text-Green-900 pr-4 text-right rounded-md"
+              className="input focus:inputFocus"
               type="text"
               placeholder="Custom"
             />
           </div>
         </section>
         <section className="mb-8">
-          <label
-            className="text-sm text-Grey-500 mb-3 block"
-            htmlFor="numPeople"
-          >
-            Number of People
-          </label>
+          <div className="flex justify-between">
+            <label
+              className="text-sm text-Grey-500 mb-3 block"
+              htmlFor="numPeople"
+            >
+              Number of People
+            </label>
+            <p className="text-red-400 text-sm hidden">Can't be zero</p>
+          </div>
           <div className="flex items-center relative  rounded-sm">
             <img className="absolute ml-4" src={iconPerson} alt="Icon person" />
             <input
-              className="text-right bg-Grey-100 rounded-md text-Green-900 pr-4 w-full h-[48px]"
+              className="input w-full h-[48px] focus:inputFocus "
               type="text"
               id="numPeople"
               placeholder="1"
             />
           </div>
         </section>
-        <section className="bg-Green-900 rounded-2xl h-[220px] flex flex-col justify-center p-6">
+        <section className="bg-Green-900 rounded-2xl h-[220px] flex flex-col justify-center p-6 mb-8">
           <div className="grid grid-cols-2 gap-7 mb-6">
             <p className="text-sm text-White flex flex-col">
               Tip Amount <span className="text-xs text-gray-400">/ person</span>
@@ -79,7 +85,5 @@ function App() {
     </>
   );
 }
-
-//Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount / person Total / person Reset
 
 export default App;
